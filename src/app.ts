3d4455016e0db03840a0139ffff4bb343c4d412e
RafaelFreitas-cloud <raf.mec.ba@gmail.com>
2023-07-28 07:50:16 -0300

    cors config (local)

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,14 @@ import { userRoutes } from "./routes/users.routes";
 import { handleErrors } from "./errors";
 import { loginRoutes } from "./routes/login.routes";
 import { contactsRoutes } from "./routes/contacts.routes";
+import cors from "cors"
 
 const app: Application = express();
 
 app.use(json());
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
 
 app.use("/users", userRoutes);
 app.use("/login", loginRoutes);
